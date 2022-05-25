286e9ff5606e7db47518aeb36f0e620cafc56b7c
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-05-25 14:40:08 -0400

    Add the needed import statements to the application.

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,8 @@
+﻿import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Inject, Injectable, PLATFORM_ID} from "@angular/core";
+import { TokenResponse } from "../interfaces/token.response";
+
+export class AuthService{
+
+}
