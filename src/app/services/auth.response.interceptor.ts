50825ea77e9715e6de919d6becf70805ed87b55a
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-05-31 21:53:16 -0400

    Update auth.response.interceptor.ts

--- src/app/services/auth.response.interceptor.ts
+++ src/app/services/auth.response.interceptor.ts
@@ -24,6 +24,9 @@ export class AuthResponseInterceptor implements HttpInterceptor{
 
   }
 
+  /**
+   * Check if a token a exist and do nothing if it does
+   */
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.auth = this.injector.get(AuthService);
     const token = (this.auth.isLoggedIn()) ? this.auth.getAuth()!.token : null;
@@ -33,13 +36,17 @@ export class AuthResponseInterceptor implements HttpInterceptor{
       this.currentRequest = request;
 
       return next.handle(request)
-        .pipe(tap((event: HttpEvent<any>) => {
-          if(event instanceof  HttpResponse){
-            console.log("The event is part of Http response");
-          }}))
-        .pipe(catchError(error => {
-          return new Observable<any>(error);
-        }));
+        .pipe(
+          tap((event: HttpEvent<any>) => {
+            if(event instanceof  HttpResponse){
+              console.log("The event is part of Http response");
+            }}
+          ),
+
+          catchError(error => {
+            return this.handleError(error);
+          })
+        );
     }
     else
     {
@@ -47,37 +54,60 @@ export class AuthResponseInterceptor implements HttpInterceptor{
     }
   }
 
-
-  handleError(err: any){
+  /**
+   * Check to make sure the error matches HttpError response with status code of 401
+   * If the error matches refresh the token using the refreshToken method.
+   * If successful we resubmit the request that triggered the response error which we saw.
+   * If it is not successful we perform a logout and clear al the expired tokens from the local storage.
+   * then redirect the user back to the login page.
+   */
+  private handleError(err: any){
     if(err instanceof HttpErrorResponse) {
       if (err.status === 401) {
 
-
         console.log("Token has expired try refreshing.....")
-        this.auth?.refreshToken()
-          .subscribe(res => {
-            if (res) {
-              console.log("refresh token successful");
-
-              const http = this.injector.get(HttpClient);
-              http.request(this.currentRequest!).subscribe(
-                result => {
-                  console.log("resubmitting the request");
-                },
-                error => {
-                  console.log(error)
-                }
-              )
-            } else {
-              console.log("refresh token failed");
-              this.auth?.logout();
-              this.router.navigate(["login"]);
-            }
-          }, error => {
-            console.log(error)
-          })
+        this.auth!.refreshToken()
+          .subscribe({
+            next: this.getNext(),
+            error: this.getError()
+          });
       }
     }
     return new Observable<any>(err);
+  }
+
+  /**
+   * Log any errors that occur.
+   */
+  private getError() {
+    return (error : any) => {
+      console.log(error)
+    };
+  }
+
+  /**
+   * Attempt to resubmit the request if that fails then log failed and return
+   * to the login page.
+   */
+  private getNext() : any  {
+    return (res: any) => {
+      if (res ) {
+        console.log("refresh token successful");
+
+        const http = this.injector.get(HttpClient);
+        http.request(this.currentRequest!).subscribe(
+          result => {
+            console.log("resubmitting the request");
+          },
+          error => {
+            console.log(error)
+          }
+        )
+      } else {
+        console.log("refresh token failed");
+        this.auth!.logout();
+        this.router.navigate(["login"]);
+      }
+    };
   }
 }
