b87cb8b9dcb4b371fc0d581991d70005a224e0f2
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-06-20 23:24:46 -0400

    Removing unwanted code from the app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
-import {AuthService} from "./services/auth.service";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AuthInterceptor} from "./services/auth.interceptor";
-import {AuthResponseInterceptor} from "./services/auth.response.interceptor";
+import { AuthService } from './services/auth.service';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './services/auth.interceptor';
+import { AuthResponseInterceptor } from './services/auth.response.interceptor';
 import { RouterModule } from '@angular/router';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule} from "./material.module";
-import {ReactiveFormsModule} from "@angular/forms";
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    LandingPageComponent
-  ],
+  declarations: [AppComponent, LoginComponent, LandingPageComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -29,24 +25,25 @@ import { LoginComponent } from './components/login/login.component';
     BrowserAnimationsModule,
     MaterialModule,
     RouterModule.forRoot([
-      {path: '', component: LoginComponent},
-      {path: 'app-landing-page', component: LandingPageComponent}
+      { path: '', component: LoginComponent, pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'landingpage', component: LandingPageComponent },
     ]),
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [
     AuthService,
     {
       provide: HTTP_INTERCEPTORS,
-      useClass : AuthInterceptor,
-      multi : true
+      useClass: AuthInterceptor,
+      multi: true,
     },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthResponseInterceptor,
-      multi: true
-    }
+      multi: true,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
