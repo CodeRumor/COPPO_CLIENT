b703963b8f4780f26edb009349715f1fa00c2798
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-06-22 03:16:22 -0400

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { NavigationEnd, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { delay, filter } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
-  selector: 'app-home-page',
+  selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private observer: BreakpointObserver
+  ) {}
+
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1), untilDestroyed(this))
+      .subscribe({
+        next: (res: any) => {
+          if (res.matches) {
+            this.sidenav.mode = 'over';
+            this.sidenav.close();
+          } else {
+            this.sidenav.mode = 'side';
+            this.sidenav.open();
+          }
+        },
+        error: (err: any) => {
+          console.log(err);
+        },
+      });
+    this.router.events
+      .pipe(
+        untilDestroyed(this),
+        filter((e) => e instanceof NavigationEnd)
+      )
+      .subscribe({
+        next: () => {
+          if (this.sidenav.mode === 'over') {
+            this.sidenav.close();
+          }
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
 
   ngOnInit(): void {}
 
