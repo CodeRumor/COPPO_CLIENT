437afd9798cab335e230b444f630f306ad2b8e6a
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-06-21 23:52:47 -0400

    Create home.component.ts

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  public logout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
+}
