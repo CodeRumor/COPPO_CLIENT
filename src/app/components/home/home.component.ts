14ce65f476fb72aa01e6262f05fa779e8a68b258
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-06-22 18:37:53 -0400

    Adding comments and removing unwanted code.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -22,45 +22,44 @@ export class HomeComponent implements OnInit {
     private observer: BreakpointObserver
   ) {}
 
+  ngOnInit(): void {}
+
+  /**
+   * This is used for instences where code might not have loaded completely.
+   */
   ngAfterViewInit() {
     this.observer
       .observe(['(max-width: 800px)'])
       .pipe(delay(1), untilDestroyed(this))
       .subscribe({
         next: (res: any) => {
-          if (res.matches) {
-            this.sidenav.mode = 'over';
-            this.sidenav.close();
-          } else {
-            this.sidenav.mode = 'side';
-            this.sidenav.open();
-          }
+          this.shouldNavSlide(res);
         },
         error: (err: any) => {
           console.log(err);
         },
       });
-    this.router.events
-      .pipe(
-        untilDestroyed(this),
-        filter((e) => e instanceof NavigationEnd)
-      )
-      .subscribe({
-        next: () => {
-          if (this.sidenav.mode === 'over') {
-            this.sidenav.close();
-          }
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
   }
 
-  ngOnInit(): void {}
-
+  /**
+   * Clear the user token from local storage and navigate to the login page.
+   */
   public logout() {
     this.authService.logout();
     this.router.navigate(['login']);
+  }
+
+  /**
+   * Determines if the slide action should be activated or not.
+   * @param res the size of the page for when the side action should be activated.
+   */
+  private shouldNavSlide(res: any) {
+    if (res.matches) {
+      this.sidenav.mode = 'over';
+      this.sidenav.close();
+    } else {
+      this.sidenav.mode = 'side';
+      this.sidenav.open();
+    }
   }
 }
