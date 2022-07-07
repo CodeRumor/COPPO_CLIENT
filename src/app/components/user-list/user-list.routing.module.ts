642a092c403aa7a7abab2724fbced72124f50fe1
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-07-07 03:05:24 -0400

    Update user-list.routing.module.ts

--- src/app/components/user-list/user-list.routing.module.ts
+++ src/app/components/user-list/user-list.routing.module.ts
@@ -2,12 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { UserAuthorisationGuard } from 'src/app/guards/user.authorisation.guard';
 import { UserListComponent } from './user-list.component';
+import { UserAdminAuthorisationGuard } from 'src/app/guards/user-admin.authorisation.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: UserListComponent,
-    canActivate: [UserAuthorisationGuard],
+    canActivate: [UserAdminAuthorisationGuard],
     data: { breadcrum: 'user-list', title: 'user-list' },
   },
 ];
