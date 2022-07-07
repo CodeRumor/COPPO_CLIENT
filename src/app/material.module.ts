d00fc493a636e37c5c6846e655dcc12d71fe8412
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-07-07 03:05:29 -0400

    Update material.module.ts

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,6 +8,7 @@ import { MatListModule } from '@angular/material/list';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 const modules = [
   MatCardModule,
@@ -17,6 +18,7 @@ const modules = [
   MatListModule,
   MatToolbarModule,
   MatIconModule,
+  MatButtonToggleModule,
 ];
 
 @NgModule({
