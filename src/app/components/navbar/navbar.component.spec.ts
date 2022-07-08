a5329491a15e56cfc92e5830230a52efeefe798c
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-07-08 18:35:51 -0400

    removing unwanted code.

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from 'src/app/services/auth.service';
+import { COMMON } from '../../common';
 
 import { NavbarComponent } from './navbar.component';
 
