6224be708928c5958dceea81c4d53bd2b3e7ad45
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-07-09 00:17:11 -0400

    Create user-detail.component.ts

---
+++ src/app/components/home/user-detail/user-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { COMMON } from '../../../common';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css'],
+})
+export class UserDetailComponent implements OnInit {
+  user: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.user = localStorage.getItem(COMMON.CURRENT_USER);
+    console.log(this.user);
+  }
+}
