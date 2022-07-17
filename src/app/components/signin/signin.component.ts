85e1e63e2a90e5b1f5523ad889a1723c84ace74c
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-07-17 15:06:59 -0400

    Update signin.component.ts

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserType } from 'src/app/interfaces/user.type';
 
 @Component({
   selector: 'app-signin',
@@ -6,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent implements OnInit {
-  constructor() {}
+  userTypes: UserType[] = [
+    { value: 'Admin', viewValue: 'Admin' },
+    { value: 'User', viewValue: 'User' },
+  ];
 
-  ngOnInit(): void {}
+  constructor(private formBuilder: FormBuilder) {}
+  createAccountForm!: FormGroup;
+  error: string = '';
+
+  ngOnInit(): void {
+    this.createAccountForm = this.formBuilder.group({
+      Username: ['', Validators.required],
+      Password: ['', Validators.required],
+      Email: ['', Validators.required],
+    });
+  }
 }
