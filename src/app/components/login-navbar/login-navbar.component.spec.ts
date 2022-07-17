bcff34e8ece64efb8d7d522c53e7b7b1151a88d9
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-07-17 17:13:43 -0400

    Create login-navbar.component.spec.ts

---
+++ src/app/components/login-navbar/login-navbar.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { LoginNavbarComponent } from './login-navbar.component';
+
+describe('LoginNavbarComponent', () => {
+  let component: LoginNavbarComponent;
+  let fixture: ComponentFixture<LoginNavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [LoginNavbarComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginNavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
