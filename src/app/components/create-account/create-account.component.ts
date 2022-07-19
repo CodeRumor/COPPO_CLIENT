5dca632f0c1b10b4eea012a28569200d0c2e0c09
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-07-19 00:20:32 -0400

    Create create-account.component.ts

---
+++ src/app/components/create-account/create-account.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserType } from 'src/app/interfaces/user.type';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './create-account.component.html',
+  styleUrls: ['./create-account.component.css'],
+})
+export class CreateAccountComponent implements OnInit {
+  userTypes: UserType[] = [
+    { value: 'Admin', viewValue: 'Admin' },
+    { value: 'User', viewValue: 'User' },
+  ];
+
+  constructor(private formBuilder: FormBuilder) {}
+  createAccountForm!: FormGroup;
+  error: string = '';
+
+  ngOnInit(): void {
+    this.createAccountForm = this.formBuilder.group({
+      Username: ['', Validators.required],
+      Password: ['', Validators.required],
+      Email: ['', Validators.required],
+    });
+  }
+}
